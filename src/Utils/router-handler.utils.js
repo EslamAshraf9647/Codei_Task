4349e72a9b653c29fb616fb132de548634e27bdc
Eslam Ashraf <eslamelmawazen300@gmail.com>
2025-04-24 17:20:38 +0200

    eight  commit

--- src/Utils/router-handler.utils.js
+++ src/Utils/router-handler.utils.js
@@ -8,9 +8,9 @@ import courseController from "../Modules/Course/course.controller.js";
 const routerhandler = (app ) => {
     app.use('/auth', authcontroller)
     app.use('/course',courseController)
-    app.get('/',(req,res) => res.status(200).json({message:"Task Done"}))
-
-
+    app.get('/', async (req,res) => {
+        return  res.status(200).json({message:'Welcome to codei task'})
+    })
 
 
     app.use(globalErrorHandler)
